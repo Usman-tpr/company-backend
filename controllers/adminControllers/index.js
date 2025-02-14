b42f90de20be28cc065954c5f07cc198c13f38a9
Usman Ali <Usman.tpr@gmail.com>
2025-02-14 18:07:30 +0500

    added login

--- controllers/adminControllers/index.js
+++ controllers/adminControllers/index.js
@@ -15,6 +15,31 @@ exports.post = async ( req , res ) => {
         sendResponse(res, 200 , false , "Error While adding admin", error.message )
     }
 }
+exports.login = async ( req , res ) => {
+    try {
+        const { email , password } = req.body
+
+        const isExist = await Admin.findOne({email});
+         if(!isExist.email){
+            return sendResponse(res , 200 , false , "Please Signup First", null)
+         }
+         else if(isExist.password !== password){
+            return sendResponse(res , 200 , false , "Invalid Credentials", null)
+         }
+         else{
+            const token = jwt.sign(
+                { userId: isExist._id },
+                process.env.JWT_SECRET,
+                { expiresIn: "30d" }
+            );
+
+            sendResponse(res, 200, true, "Login Successfully", {isExist , token});
+         }
+   
+    } catch (error) {
+        sendResponse(res, 200 , false , "Error While adding admin", error.message )
+    }
+}
 
 exports.get = async ( req , res ) => {
          try {
--- routes/v1/adminRoutes.js
+++ routes/v1/adminRoutes.js
@@ -1,9 +1,10 @@
 const route = require("express").Router();
-const { post , get } = require("../../controllers/adminControllers");
+const { post , get , login } = require("../../controllers/adminControllers");
 const authMiddleware = require("../../middlewares/authMiddleware");
 const { isAdmin } = require("../../middlewares/isAdmin");
 
 route.post("/add" , post)
-route.get("/get", get )
+route.post("/login" , login)
+route.get("/get", get)
 
 module.exports = route
